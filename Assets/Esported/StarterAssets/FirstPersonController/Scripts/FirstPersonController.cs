c5ec97f855ca360243865154a1839018693957a9
PedroHillesheim <pedro.hillesheim@alunos.sc.senac.br>
2026-07-07 11:16:33 -0300

    .

--- Assets/Esported/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/Esported/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -101,6 +101,7 @@ namespace StarterAssets
 			_input = GetComponent<StarterAssetsInputs>();
 #if ENABLE_INPUT_SYSTEM
 			_playerInput = GetComponent<PlayerInput>();
+			Cursor.lockState = CursorLockMode.Locked;
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
